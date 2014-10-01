c34a398d364bda453355ce3750b64f66e8defd29
Jeremy Fox <foxjerem@gmail.com>
2014-10-01 15:54:39 +0100

    Updated validations

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,7 @@
 class Workout < ActiveRecord::Base
 
-  INTENSITY = %w(bruce lee high medium low)
+  INTENSITIES = %w(bruce lee high medium low)
+  METRICS     = %w(min/km min/mile)
 
   validates :activity,  presence:     { message: 'a workout must have an activity' }
   
@@ -10,10 +11,19 @@ class Workout < ActiveRecord::Base
   validates :duration,  numericality: { message: 'duration must be a number' }
   
   validates :intensity, presence:     { message: 'a workout must have an intensity' }
-  validates :intensity, inclusion:    { in: INTENSITY , message: 'intensity must be one of bruce lee, high, medium, low' }
+  validates :intensity, inclusion:    { in: INTENSITIES , message: 'intensity must be one of bruce lee, high, medium, low' }
+
+  validate :if_paced_has_metric,        message: 'a paced workout must have a metric' 
+  validates :metric, inclusion:       { in: METRICS , message: 'metric must be either min/km or min/mile' }
 
   def flash_error
     errors.messages.map{ |msg_key, msg_val| "Error: #{msg_val.join(',')}\n" }.join('')
+  end
+
+  def if_paced_has_metric
+    if pace.present? && !metric.present?
+      errors.add(:metric, 'a paced workout must have a metric' ) 
+    end
   end
 
   class << self
--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -33,9 +33,21 @@ RSpec.describe Workout, :type => :model do
   end
 
   it 'the intensity must be either bruce lee, high, medium, low' do
-    workout = Workout.new(activity: 'Running', date: '10/12/2014', duration: 'xyz', intensity: 'blah') 
+    workout = Workout.new(activity: 'Running', date: '10/12/2014', duration: 35, intensity: 'blah') 
     expect(workout).not_to be_valid
     expect(workout.errors.messages[:intensity]).to include('intensity must be one of bruce lee, high, medium, low')
+  end
+
+  it 'the metric must be either min/km or min/mile' do
+    workout = Workout.new(activity: 'Running', date: '10/12/2014', duration: 35, intensity: 'high', pace: 4, metric: 'hi') 
+    expect(workout).not_to be_valid
+    expect(workout.errors.messages[:metric]).to include('metric must be either min/km or min/mile')
+  end
+
+  it 'if a workout has a pace it must have a metric' do
+    workout = Workout.new(activity: 'Running', date: '10/12/2014', duration: 35, intensity: 'high', pace: 4) 
+    expect(workout).not_to be_valid
+    expect(workout.errors.messages[:metric]).to include('a paced workout must have a metric')
   end
 
 end
