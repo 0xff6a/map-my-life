f9b74f50246089a18fbd6501f6c9bdd28709ef28
Jeremy Fox <foxjerem@gmail.com>
2014-10-16 10:57:16 +0100

    updated vo2 max method

--- app/lib/run_analyzer.rb
+++ app/lib/run_analyzer.rb
@@ -10,6 +10,39 @@ class RunAnalyzer
       [1, (target.pace / workout.pace)].min * 100
     end
 
+    def predicted_time(distance, guess = 500)
+      tolerance = 0.1
+      t_min, t_max = 0, 1500  
+      diff = benchmark_vo2_max - vo2_max(distance, guess)
+      return guess if diff.abs < tolerance
+      diff > 0 ? predicted_time(distance, 0.5*(guess + t_max)) : predicted_time(distance, 0.5*(guess + t_min))
+    end
+
+    def benchmark_vo2_max
+      vo2_max(@benchmark.duration, @benchmark.distance)
+    end
+
+    def vo2_max(time, distance)
+      vo2(speed(time, distance)) / pct_max(time)
+    end
+
+    private
+
+    # formulae taken from http://www.simpsonassociatesinc.com/
+
+    def pct_max(time)
+      0.8+(0.1894393*Math.exp(-0.012778*time))+(0.2989558*Math.exp(-0.1932605*time));
+    end
+
+    def vo2(speed)
+      -4.60+(0.182258*speed)+(0.000104*speed*speed)
+    end
+
+    def speed(time, distance)
+      distance * 1000 / time
+    end
+
   end
 
 end
+
--- spec/lib/run_analyzer_spec.rb
+++ spec/lib/run_analyzer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe RunAnalyzer do
   
-  let(:workout) { FactoryGirl.create(:workout, distance: 5, pace: 4.00) }
+  let(:workout) { FactoryGirl.create(:workout, distance: 5, pace: 4.00, duration: 20) }
   let(:target)  { FactoryGirl.create(:target, distance: 5, pace: 4.00)  }
 
   context 'equal distance comparison' do
@@ -23,7 +23,7 @@ describe RunAnalyzer do
 
   end
 
-  context 'VDOT benchmark' do
+  context 'VO2 Max benchmark' do
 
     it 'should not have a benchmark intially' do
       expect(RunAnalyzer.benchmark).to be nil
@@ -34,8 +34,19 @@ describe RunAnalyzer do
       expect(RunAnalyzer.benchmark).to eq workout
     end
 
-    it 'can calculate a VDOT from the benchmark' do
-      expect(RunAnalyzer.vdot).to eq 2 
+    it 'can calculate a VO2 Max from the benchmark' do
+      RunAnalyzer.benchmark = workout
+      expect(RunAnalyzer.benchmark_vo2_max.round(2)).to eq 49.81
+    end
+
+  end
+
+  context 'unequal distance comparison' do
+
+    before(:each) { RunAnalyzer.benchmark = workout }
+
+    it 'should get a predicted runtime for a given distance from benchmark' do
+      expect(RunAnalyzer.predicted_time(10.0)).to eq 45
     end
 
   end
