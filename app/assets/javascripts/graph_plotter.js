1186a278aff360cca5c679e8e51ef4e0559c1748
Jeremy Fox <foxjerem@gmail.com>
2014-10-23 11:21:21 +0100

    Rejigged routes

--- app/assets/javascripts/graph_plotter.js
+++ app/assets/javascripts/graph_plotter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var $chartHolder = $('#placeholder');
 
-  $.get( '/graphs/graph_data', function(response) {
+  $.get( '/runs/graph_data', function(response) {
     $.plot($chartHolder, [response], 
             { 
               xaxis: {mode: 'time', timeformat: '%d/%m/%y'} 
